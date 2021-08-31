a5d2681ef0b29c8a9c9fa6e27d0105aa0c41f18e
MTandHJ <congxueric@gmail.com>
2021-08-31 21:14:42 +0800

    modify mnist

--- models/mnist.py
+++ models/mnist.py
@@ -18,28 +18,28 @@ class MNIST(AdversarialDefensiveModel):
         self.conv = nn.Sequential( # 1 x 28 x 28
             nn.Conv2d(1, 32, 3),   # 32 x 26 x 26
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.Conv2d(32, 32, 3),  # 32 x 24 x 24
             nn.BatchNorm2d(32),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.MaxPool2d(2),       # 32 x 12 x 12
             nn.Conv2d(32, 64, 3),  # 64 x 10 x 10
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.Conv2d(64, 64, 3),  # 64 x 8 x 8
             nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.MaxPool2d(2)        # 64 x 4 x 4
         )
 
         self.dense = nn.Sequential(
             nn.Linear(64 * 4 * 4, 200),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.BatchNorm1d(200),
             nn.Linear(200, dim_feature),
             nn.BatchNorm1d(dim_feature)
         )
-        self.activation = nn.ReLU(inplace=True)
+        self.activation = nn.PReLU()
         self.fc = nn.Linear(dim_feature, num_classes)
         _weights = generate_weights(dim_feature)[:num_classes] * scale
         self.fc.weight.data.copy_(_weights)
